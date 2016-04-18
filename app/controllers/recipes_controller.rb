8d8ae6894b19e16a93e43dfad667bf5ed2e7adc9
Ali Zaidi <msazaidi@gmail.com>
2016-04-18 08:44:59 -0700

    Completed Authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,13 @@
 class RecipesController < ApplicationController
-  
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
     @recipes = Recipe.paginate(page: params[:page], :per_page => 5)
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
   
   def new
@@ -14,7 +16,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(1)
+    @recipe.chef = current_user
 
     if @recipe.save
     	flash[:success] = "Your Recipe was Created Succesfully"
@@ -25,11 +27,9 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
     	redirect_to recipes_path(@recipe)
     	flash[:success] = "Your Recipe was Edited Successfully"
@@ -39,8 +39,7 @@ class RecipesController < ApplicationController
   end
 
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was succesful"
       redirect_to :back
@@ -53,5 +52,16 @@ class RecipesController < ApplicationController
   private
     def recipe_params
       params.require(:recipe).permit(:name,:summary,:description, :picture)
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @recipe.chef
+        flash[:danger] = "You can only edit your own recipes"
+        redirect_to recipes_path
+      end
     end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -7,5 +7,7 @@ class Chef < ActiveRecord::Base
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 }, 
-            uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }     
+            uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password     
 end
